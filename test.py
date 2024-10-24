d2cb98d51a906205c81ddba1f307872761ba0c6d
K4_keyChan_ <kanva.avnak@gmail.com>
2024-10-24 12:38:48 +0530

    upluaded and split data

--- test.py
+++ test.py
@@ -4,16 +4,18 @@ data = pd.read_csv('rba-dataset.csv')
 
 print(data.head())
 
-split = [None for i in range(20)]
-j1, j2 = 0, int(len(data)/20)
+split = [None for i in range(24)]
+j1, j2 = 0, int(len(data)/24)
 print(len(data))
 print(j1, j2)
-# for i in range(20):
-#     split[i] = data.iloc[j1:j1 + j2]
-#     j1 += j2
+input('press enter to continue')
 
-# print('done splitting')
+for i in range(24):
+    split[i] = data.iloc[j1:j1 + j2]
+    j1 += j2
 
-# for i in range(20):
-#     split[i].to_csv('rba-split-{i}.csv', index=False)
-#     print(split[i].head())
+print('done splitting')
+
+for i in range(24):
+    split[i].to_csv(f'rba-split-{i}.csv', index=False)
+    print(split[i].head())
